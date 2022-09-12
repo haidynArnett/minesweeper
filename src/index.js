db6dc67abffb120ec5473564778e8d48130b4544
haidynArnett <harnett7@gatech.edu>
2022-09-12 14:21:40 -0400

    Revert "Updates"
    This reverts commit 83aa335d9ea1d7855030fb030acabbd7eea02500.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import Minesweeper from './Minesweeper/Minesweeper.js';
 import reportWebVitals from './reportWebVitals';
 
-var root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(
-  React.StrictMode,
-  null,
-  React.createElement(Minesweeper, null)
-));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Minesweeper />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-export { default as Minesweeper } from './Minesweeper/Minesweeper.js';
+export {default as Minesweeper} from './Minesweeper/Minesweeper.js';
